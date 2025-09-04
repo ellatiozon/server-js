3c8015aab322e5fe9d12d7d70bcc054797145bd4
ella-mari <juppai@example.com>
2025-09-04 15:16:08 +0800

    Updated file server features

--- server.js
+++ server.js
@@ -2,8 +2,52 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime-types');
+const formidable = require('formidable');
+
+//checking upload directory
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir);
+}
 
 const server = http.createServer((req, res) => {
+    if (req.url == "/upload" && req.method.toLowerCase() === 'post') {
+        const form = new formidable({
+            multiples: false,
+            uploadDir: uploadDir,
+            keepExtensions: true,
+        });
+
+        form.onPart = function(part) {
+            if (part.filename) {
+                const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+                const ext = path.extname(part.filename).toLowerCase();
+
+                if (!allowedTypes.includes(ext)){
+                    res.writeHead(400, { 'Content-Type': 'text/plain' });
+                    res.end('<H1>Invalid file type. Only accepts JPG, PNG, and GIF are allowed.</H1>', 'utf8');
+                    return;
+                }
+            }
+
+            form.handlePart(part);
+        };
+
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('<H1>Server Error during file upload</H1>', 'utf8');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('<H1>File uploaded successfully!</H1>', 'utf8');
+        });
+
+    }else{
+        let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    }
+
+
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 
     fs.readFile(filePath, (err, stats) => {
